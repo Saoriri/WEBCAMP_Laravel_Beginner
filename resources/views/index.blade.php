5c537b614f39c962d129976aec729bb6327c45c3
Saoriri <Saoriri@example.com>
2023-04-14 11:52:33 +0000

    非ログインTopPageとログイン後TopPageを作成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,11 @@
-<!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>ログイン機能付きタスク管理サービス</title>
-    </head>
+@extends('layout')
 
-    <body>
+{{-- メインコンテンツ --}}
+@section('contents')
         <h1>ログイン</h1>
         <form action="/login" method="post">
-            email:<input><br>
-            パスワード:<input type="password"><br>
+            email：<input name="email"><br>
+            パスワード：<input  name="password" type="password"><br>
             <button>ログインする</button>
         </form>
-    </body>
-</html>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +16,10 @@ use App\Http\Controllers\AuthController;
 |
 */
 
+//　タスク管理システム
 Route::get('/', [AuthController::class,'index']);
+Route::get('/task/list',[TaskController::class,'list']);
 
+// テスト用
 Route::get('/welcome', [WelcomeController::class, 'index']);
 Route::get('/welcome/second', [WelcomeController::class, 'second']);
