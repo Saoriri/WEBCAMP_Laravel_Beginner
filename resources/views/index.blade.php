aaaf424ead8fb36f9ef883971932923857e0ecf8
Saoriri <Saoriri@example.com>
2023-04-16 14:17:29 +0000

    認証の「formデータを受け取る」所まで書いた

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 {{-- メインコンテンツ --}}
-@section('contents')
+@section('contets')
         <h1>ログイン</h1>
         @if ($errors->any())
             <div>
@@ -11,6 +11,7 @@
             </div>
         @endif
         <form action="/login" method="post">
+            @csrf
             email：<input name="email" value="{{ old('email') }}"><br>
             パスワード：<input  name="password" type="password"><br>
             <button>ログインする</button>
