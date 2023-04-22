4cc978660d6ec6bc46f4548b98b0b7a4d52c5136
Saoriri <Saoriri@example.com>
2023-04-22 12:23:24 +0000

    タスクの一覧出力を書いた

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,7 +7,7 @@ use App\Http\Requests\TaskRegisterPostRequest;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Task as TaskModel;
 
-class TaskController  extends Controller
+class TaskController extends Controller
 {
     /**
      * タスク一覧ページ を表示する
@@ -16,7 +16,23 @@ class TaskController  extends Controller
      */
     public function list()
     {
-        return view('task.list');
+        // 一覧の取得
+        $list = TaskModel::where('user_id', Auth::id())
+                         ->orderBy('priority', 'DESC')
+                         ->orderBy('period')
+                         ->orderBy('created_at')
+                         ->get();
+/*
+$sql = TaskModel::where('user_id', Auth::id())
+                 ->orderBy('priority', 'DESC')
+                 ->orderBy('period')
+                 ->orderBy('created_at')
+                 ->toSql();
+//echo "<pre>\n"; var_dump($sql, $list); exit;
+var_dump($sql);
+*/
+        //
+        return view('task.list', ['list' => $list]);
     }
 
     /**
