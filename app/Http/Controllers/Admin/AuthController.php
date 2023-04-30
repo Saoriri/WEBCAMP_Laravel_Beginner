381bd13fdc2338e9a5d44863dd779d8ea04ef589
Saoriri <Saoriri@example.com>
2023-04-30 06:49:40 +0000

    質問フォーム　RE:ログインformから入力データを受け取るの挙動確認について

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -1,9 +1,9 @@
 <?php
 declare(strict_types=1);
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\LoginPostRequest;
+use App\Http\Requests\AdminLoginPostRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
@@ -16,21 +16,22 @@ class AuthController extends Controller
      */
     public function index()
     {
-        return view('admin.index');
+        return view('index');
     }
 
-    /**
+     /**
      * ログイン処理
      * 
      */
-    public function login(LoginPostRequest $request)
+    public function login(AdminLoginPostRequest $request)
     {
         // validate済
 
         // データの取得
         $datum = $request->validated();
-        //var_dump($datum); exit;
+        var_dump($datum); exit;
 
+        /*
         // 認証
         if (Auth::attempt($datum) === false) {
             return back()
@@ -42,6 +43,7 @@ class AuthController extends Controller
         //
         $request->session()->regenerate();
         return redirect()->intended('/task/list');
+        */
     }
 
     /**
